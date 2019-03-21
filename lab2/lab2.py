9c1b52052ddd7ec69d6fbad1125d6d2f71f8eb81
Jakub Tomczak <jakub.tomczak@live.com>
2019-03-21 19:40:35 +0100

    Check legal combinations of front.

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -25,9 +25,23 @@ num_to_state_mapping = {
 # [north, east, south, west]
 neighbors_coordinates = [(-1, 0), (0, 1), (1, 0), (0, -1)]
 
+
 def debug_print(text):
     if not optilio_mode:
         print(text)
+
+
+def display_front(row, col, front, test_case):
+    print("{}front for row:{} col:{}{}".format('*' * 5, row, col, '*' * 5))
+    front_n = np.zeros(test_case.size)
+    front_n[row, col] = 'X'
+    for fr in front.cells_in_front:
+        front_n[fr.row, fr.col] = '?'
+        for breeze in fr.breezes_in_neighborhood:
+            front_n[breeze[0], breeze[1]] = 'B'
+
+    print(front_n)
+    print("{} end front {}".format('*' * 5, '*' * 5))
 
 
 class Front:
@@ -36,6 +50,9 @@ class Front:
         self.breezes = set()
         for cell in self.cells_in_front:
             self.breezes.add(cell.breezes_in_neighborhood)
+
+    def __len__(self):
+        return len(self.cells_in_front)
 
     # add cell with all its breezes,
     # merge the existing breezes set with cell's breezes
@@ -53,11 +70,13 @@ class CellInFront:
         self.col = col
         self.breezes_in_neighborhood = set()
 
+    def __str__(self):
+        return 'row:{} col:{}, breezes{}'.format(self.row, self.col, self.breezes_in_neighborhood)
+
 
 class TestCase:
     # test_case.size is a real size of the input data
     # test_case.data_matrix_size may be equal to test_case.size or may have 1 cell offset
-
     def __init__(self, optilio_mode):
         self.size = (0, 0)
         self.data_matrix_size = (0, 0)
@@ -78,7 +97,7 @@ class TestCase:
     def parse_test_case(self, lines, extended):
         if extended:
             self.extended_data_matrix = True
-            self.data_matrix_size = (self.size[0]+2, self.size[1]+2)
+            self.data_matrix_size = (self.size[0] + 2, self.size[1] + 2)
             self.data = np.zeros(self.data_matrix_size)
             self.raw_data = self.data[1:-1, 1:-1]
         else:
@@ -105,6 +124,7 @@ class TestCase:
         except:
             pass
 
+
 def parse_test_data(dir, filename, optilio_mode, extended_data_matrix):
     import os
     in_path = '{}/{}/{}.in'.format(os.getcwd(), dir, filename)
@@ -126,15 +146,16 @@ def parse_test_data(dir, filename, optilio_mode, extended_data_matrix):
             case.parse_test_case(lines, extended_data_matrix)
             test_cases.append(case)
 
-
     try:
         with open(out_path, 'r') as file:
             for case in test_cases:
-                case.ground_truth = np.array([[float(x) for x in file.readline().split(' ')] for _ in range(case.size[0])])
+                case.ground_truth = np.array(
+                    [[float(x) for x in file.readline().split(' ')] for _ in range(case.size[0])])
     except:
         pass
 
     return test_cases
+
 
 def parse_test_data_from_input(optilio_mode, extended_data_matrix):
     no_test_cases = int(input())
@@ -154,10 +175,31 @@ def parse_test_data_from_input(optilio_mode, extended_data_matrix):
     return test_cases
 
 
+# finds all breezes around cell and returns this cell encapsulated in a class
+def element_to_front(row, col, test_case):
+    element = CellInFront(row, col)
+
+    # add breezes in neighborhood to an element
+    if row - 1 >= 0 and test_case.raw_data[row - 1, col] == state_to_num_mapping['B']:
+        element.breezes_in_neighborhood.add((row - 1, col))
+    if row + 1 < test_case.size[0] and test_case.raw_data[row + 1, col] == state_to_num_mapping['B']:
+        element.breezes_in_neighborhood.add((row + 1, col))
+    if col - 1 >= 0 and test_case.raw_data[row, col - 1] == state_to_num_mapping['B']:
+        element.breezes_in_neighborhood.add((row, col - 1))
+    if col + 1 < test_case.size[1] and test_case.raw_data[row, col + 1] == state_to_num_mapping['B']:
+        element.breezes_in_neighborhood.add((row, col + 1))
+
+    return element
+
+
 # termination indicates whether we came from `B`
 def find_front(row, col, front, test_case, previous):
     previous_sym, previous_coords = previous
-    debug_print("find_front: y:{}, x:{}, what? {}, current front: {}, previous? {}".format(row, col, num_to_state_mapping[test_case.raw_data[row, col]], front, previous))
+    debug_print("find_front: y:{}, x:{}, what? {}, current front: {}, previous? {}".format(row, col,
+                                                                                           num_to_state_mapping[
+                                                                                               test_case.raw_data[
+                                                                                                   row, col]], front,
+                                                                                           previous))
     pos = np.zeros(test_case.size)
     pos[row, col] = 1
     debug_print(pos)
@@ -165,7 +207,9 @@ def find_front(row, col, front, test_case, previous):
         debug_print('{}, {} is already during checking'.format(row, col))
         return
 
-    if test_case.visited[row, col] and (test_case.raw_data[row, col] == state_to_num_mapping['O'] or test_case.raw_data[row, col] == state_to_num_mapping['?']):
+    if test_case.visited[row, col] and (
+                    test_case.raw_data[row, col] == state_to_num_mapping['O'] or test_case.raw_data[row, col] ==
+                state_to_num_mapping['?']):
         debug_print('{}, {} is visited and is O or ?'.format(row, col))
         return
 
@@ -178,20 +222,20 @@ def find_front(row, col, front, test_case, previous):
     if num_to_state_mapping[previous_sym] == 'B':
         # came from 'B' and i am '?'
         debug_print('{}, {} i am ?, you came from B, I should be in the front'.format(row, col))
-        front.append((row, col))
+        front.add_cell(element_to_front(row, col, test_case))
 
     next_previous = (test_case.raw_data[row, col], (row, col))
     # go left
-    if col > 0 and col-1 != previous_coords[1] and row!=previous_coords[0]:
+    if col > 0 and col - 1 != previous_coords[1] and row != previous_coords[0]:
         find_front(row, col - 1, front, test_case, next_previous)
     # go right
-    if col + 1 < test_case.width and col+1 != previous_coords[1] and row!=previous_coords[0]:
+    if col + 1 < test_case.width and col + 1 != previous_coords[1] and row != previous_coords[0]:
         find_front(row, col + 1, front, test_case, next_previous)
     # go up
-    if row > 0 and col != previous_coords[1] and row-1!=previous_coords[0]:
+    if row > 0 and col != previous_coords[1] and row - 1 != previous_coords[0]:
         find_front(row - 1, col, front, test_case, next_previous)
     # go down
-    if row + 1 < test_case.height and col != previous_coords[1] and row+1!=previous_coords[0]:
+    if row + 1 < test_case.height and col != previous_coords[1] and row + 1 != previous_coords[0]:
         find_front(row + 1, col, front, test_case, next_previous)
 
 
@@ -206,7 +250,7 @@ def preprocessing(test_case):
     test_case.visited[mask] = True
 
     # use mask with an offset to avoid corner case checking
-    mask = np.full((test_case.size[0]+2, test_case.size[1]+2), False)
+    mask = np.full((test_case.size[0] + 2, test_case.size[1] + 2), False)
 
     for row in range(1, test_case.data_matrix_size[0] - 1):
         for column in range(1, test_case.data_matrix_size[1] - 1):
@@ -225,7 +269,7 @@ def preprocessing(test_case):
     for row in range(1, test_case.data_matrix_size[0] - 1):
         for column in range(1, test_case.data_matrix_size[1] - 1):
             sum_around = test_case.data[row - 1, column] + test_case.data[row, column + 1] + \
-                test_case.data[row, column - 1] + test_case.data[row + 1, column]
+                         test_case.data[row, column - 1] + test_case.data[row + 1, column]
             # too distant `?`
             if test_case.data[row, column] == state_to_num_mapping['?']:
 
@@ -233,33 +277,61 @@ def preprocessing(test_case):
                 if sum_around == 0:
                     test_case.visited[row - 1, column - 1] = True
 
-                # around `?` there are at least 3 `B`
-                # TO DO maybe consider only 4 B
-                # this is not correct
-                # elif sum_around == 3 or sum_around == 4:
-                #     test_case.visited[row - 1, column - 1] = True
-                #     test_case.probabilities[row - 1, column - 1] = 1.0
+                    # around `?` there are at least 3 `B`
+                    # TO DO maybe consider only 4 B
+                    # this is not correct
+                    # elif sum_around == 3 or sum_around == 4:
+                    #     test_case.visited[row - 1, column - 1] = True
+                    #     test_case.probabilities[row - 1, column - 1] = 1.0
 
-            elif test_case.data[row, column] == state_to_num_mapping['B'] and sum_around == 3*state_to_num_mapping['O']:
+            elif test_case.data[row, column] == state_to_num_mapping['B'] and sum_around == 3 * state_to_num_mapping[
+                'O']:
                 for coords in neighbors_coordinates:
                     if test_case.data[row + coords[0], column + coords[1]] == state_to_num_mapping['?']:
                         test_case.visited[row + coords[0] - 1, column + coords[1] - 1] = True
                         test_case.probabilities[row + coords[0] - 1, column + coords[1] - 1] = 1.0
 
+
 def calculate_probabilities(test_case):
     preprocessing(test_case)
 
     row = 0
-    col = 4
+    col = 0
     row = row + 1
     col = col + 1
-    front = []
+    front = Front([])
     test_case.during_check = np.zeros(test_case.size, dtype=bool)
-    find_front(row-1, col-1, front, test_case, (-1, (-1, -1)))
-    neighbors_sum = test_case.data[row-1, col] + test_case.data[row, col - 1] + test_case.data[row, col+1] + test_case.data[row+1, col]
-    if len(front) == 0 and neighbors_sum > 0 and neighbors_sum < 5:
-        test_case.probabilities[row-1, col-1] = 1.0
-        test_case.visited[row-1, col-1] = True
+    find_front(row - 1, col - 1, front, test_case, (-1, (-1, -1)))
+
+    # set probability == 1 to all '?' without front and with 'B' as neighbor
+    neighbors_sum = test_case.data[row - 1, col] + test_case.data[row, col - 1] + test_case.data[row, col + 1] + \
+                    test_case.data[row + 1, col]
+    if len(front) == 0 and 0 < neighbors_sum < 5:
+        test_case.probabilities[row - 1, col - 1] = 1.0
+        test_case.visited[row - 1, col - 1] = True
+
+    # display front
+    if not optilio_mode:
+        display_front(row, col, front, test_case)
+
+
+    cell_to_binary_position_mapping = dict()
+    for cell, o in zip(front.cells_in_front, range(len(front))):
+        debug_print('{}, {}: {},\tbreezes {}'.format(cell.row, cell.col, 1 << o, cell.breezes_in_neighborhood))
+        cell_to_binary_position_mapping[cell] = 1 << o
+
+    legal_combinations = []
+    num_of_combinations = 1 << len(front)
+    for i in range(1, 1 + num_of_combinations):
+        new_breeze = set()
+        for f in front.cells_in_front:
+            if cell_to_binary_position_mapping[f] & i:
+                new_breeze |= f.breezes_in_neighborhood
+                if len(new_breeze) == len(front.get_breezes()):
+                    debug_print("legal combination is {}".format(i))
+                    legal_combinations.append(i)
+                    break
+
     return
 
     # calculate probabilities
@@ -278,14 +350,16 @@ def calculate_probabilities(test_case):
                 print(test_case.probabilities[row, column], end=' ')
             print()
 
-filename = '2015' #2019_00_small
+
+filename = 'file'  # 2019_00_small
+
 
 def main():
     tests = []
     if optilio_mode:
-        tests = parse_test_data_from_input(optilio_mode, extended_data_matrix = True)
+        tests = parse_test_data_from_input(optilio_mode, extended_data_matrix=True)
     else:
-        tests = parse_test_data(dir, filename, optilio_mode, extended_data_matrix = True)
+        tests = parse_test_data(dir, filename, optilio_mode, extended_data_matrix=True)
 
     if optilio_mode:
         [calculate_probabilities(test_case) for test_case in tests]
@@ -294,13 +368,14 @@ def main():
         # print(test_case.data[1:-1, 1:-1])
         calculate_probabilities(test_case)
 
-        print('{}summary{}'.format('-'*10, '-'*10))
+        print('{}summary{}'.format('-' * 10, '-' * 10))
         print(test_case.visited)
         print(test_case.probabilities)
         print()
         print(test_case.ground_truth)
         print()
         test_case.check_differences()
+
 
 if __name__ == "__main__":
     main()
